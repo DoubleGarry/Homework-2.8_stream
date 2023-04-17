a7a5e1c7429bc6738d515baf3ff8656d03e918bd
semenikhin <garry@nextmail.ru>
2023-04-17 22:26:29 +0300

    Second commit

--- src/main/java/pro/sky/homework_stream/controllers/EmployeeController.java
+++ src/main/java/pro/sky/homework_stream/controllers/EmployeeController.java
@@ -2,20 +2,51 @@ package pro.sky.homework_stream.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.homework_stream.model.Employee;
 import pro.sky.homework_stream.services.EmployeeService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+
     private final EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-    @GetMapping
-    public String hello() {
-        return employeeService.hello();
+
+    @GetMapping("/add")
+    public Employee addEmployee(@RequestParam("firstName") String firstName,
+                        @RequestParam("lastName") String lastName,
+                        @RequestParam("department") int department,
+                        @RequestParam("salary") int salary) {
+        return employeeService.addEmployee(firstName, lastName, department, salary);
     }
+
+    @GetMapping("/remove")
+    public Employee removeEmployee(@RequestParam("firstName") String firstName,
+                           @RequestParam("lastName") String lastName,
+                           @RequestParam("department") int department,
+                           @RequestParam("salary") int salary) {
+        return employeeService.removeEmployee(firstName, lastName, department, salary);
+    }
+
+    @GetMapping("/find")
+    public Employee findEmployee(@RequestParam("firstName") String firstName,
+                         @RequestParam("lastName") String lastName,
+                         @RequestParam("department") int department,
+                         @RequestParam("salary") int salary) {
+        return employeeService.findEmployee(firstName, lastName, department, salary);
+    }
+
+    @GetMapping
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
 }
 
--- src/main/java/pro/sky/homework_stream/services/EmployeeService.java
+++ src/main/java/pro/sky/homework_stream/services/EmployeeService.java
@@ -1,10 +1,63 @@
 package pro.sky.homework_stream.services;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import pro.sky.homework_stream.exceptions.EmployeeAlreadyAddedException;
+import pro.sky.homework_stream.exceptions.EmployeeNotFoundException;
+import pro.sky.homework_stream.exceptions.EmployeeStorageIsFullException;
+import pro.sky.homework_stream.model.Employee;
+
 
 @Service
 public class EmployeeService {
-    public String hello() {
-        return "<b>Привет</b>";
+
+    private static final int LIMIT = 10;
+
+    private final List<Employee> employees = new ArrayList<>();
+
+    public Employee addEmployee(String firstName,
+                        String lastName,
+                        int department,
+                        int salary) {
+        Employee employee = new Employee(firstName, lastName, department, salary);
+        if (employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException();
+        }
+        if (employees.size() < LIMIT) {
+            employees.add(employee);
+            return employee;
+        }
+        throw new EmployeeStorageIsFullException();
     }
+
+    public Employee removeEmployee(String firstName,
+                           String lastName,
+                           int department,
+                           int salary) {
+        Employee employee = new Employee(firstName, lastName, department, salary);
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
+        }
+        employees.remove(employee);
+        return employee;
+    }
+
+    public Employee findEmployee(String firstName,
+                         String lastName,
+                         int department,
+                         int salary) {
+        Employee employee = new Employee(firstName, lastName, department, salary);
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
+        }
+        return employee;
+    }
+
+    public List<Employee> findAll() {
+        return new ArrayList<>(employees);
+    }
+
 }
